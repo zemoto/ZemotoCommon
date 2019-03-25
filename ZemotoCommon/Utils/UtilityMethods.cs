3d193917250ff323413115574e25b57756e20033
David Markachev <zemoto@gmail.com>
2019-03-25 11:20:00 -0400

    +Directory utility methods

--- ZemotoCommon/Utils/UtilityMethods.cs
+++ ZemotoCommon/Utils/UtilityMethods.cs
@@ -16,6 +16,26 @@ namespace ZemotoCommon.Utils
          }
       }
 
+      public static void SafeDeleteDirectory( string dirPath )
+      {
+         if ( Directory.Exists( dirPath ) )
+         {
+            try
+            {
+               Directory.Delete( dirPath, true );
+            }
+            catch { /*ignored*/ }
+         }
+      }
+
+      public static void CreateDirectory( string dirPath )
+      {
+         if ( !Directory.Exists( dirPath ) )
+         {
+            Directory.CreateDirectory( dirPath );
+         }
+      }
+
       public static string MakeUniqueFileName( string filePath )
       {
          string dir = Path.GetDirectoryName( filePath );
