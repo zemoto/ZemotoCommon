803c69d4a34ab2fb62a5a4b78930dde26a1b24cb
Unknown <zemoto@gmail.com>
2019-03-26 00:39:29 -0400

    +PathUtils and some methods for moving/renaming files

---
+++ ZemotoCommon/Utils/PathUtils.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace ZemotoCommon.Utils
+{
+   public static class PathUtils
+   {
+      public static string MoveFileToFolder( string filePath, string targetDir )
+      {
+         var fileName = Path.GetFileName( filePath );
+         var target = Path.Combine( targetDir, fileName );
+         File.Move( filePath, target );
+         return target;
+      }
+
+      public static string RenameFile( string filePath, string newName )
+      {
+         var dir = Path.GetDirectoryName( filePath );
+         var newFilePath = Path.Combine( dir, newName ) + Path.GetExtension( filePath );
+
+         if ( File.Exists( newFilePath ) )
+         {
+            File.Delete( newFilePath );
+         }
+
+         File.Move( filePath, newFilePath );
+         return newFilePath;
+      }
+   }
+}
