428738f34dc209d6397472b2f8accfb08d22d4bd
David <none@none.com>
2020-05-10 11:10:54 -0400

    Add exception defense to PathUtils

--- ZemotoCommon/Utils/PathUtils.cs
+++ ZemotoCommon/Utils/PathUtils.cs
@@ -4,46 +4,73 @@ namespace ZemotoCommon.Utils
 {
    public static class PathUtils
    {
-      public static string MoveFileToFolder( string filePath, string targetDir )
+      public static bool MoveFileToFolder( string filePath, string targetDir, out string newPath )
       {
-         var fileName = Path.GetFileName( filePath );
-         var target = Path.Combine( targetDir, fileName );
-
-         if ( !File.Exists( target ) )
+         try
          {
-            File.Delete( target );
-         }
+            var fileName = Path.GetFileName( filePath );
+            var target = Path.Combine( targetDir, fileName );
 
-         File.Move( filePath, target );
-         return target;
+            if ( !File.Exists( target ) )
+            {
+               File.Delete( target );
+            }
+
+            File.Move( filePath, target );
+            newPath = target;
+            return true;
+         }
+         catch
+         {
+            newPath = string.Empty;
+            return false;
+         }
       }
 
-      public static string CopyFileToFolder( string filePath, string targetDir )
+      public static bool CopyFileToFolder( string filePath, string targetDir, out string newPath )
       {
-         var fileName = Path.GetFileName( filePath );
-         var target = Path.Combine( targetDir, fileName );
-
-         if ( File.Exists( target ) )
+         try
          {
-            File.Delete( target );
-         }
+            var fileName = Path.GetFileName( filePath );
+            var target = Path.Combine( targetDir, fileName );
 
-         File.Copy( filePath, target );
-         return target;
+            if ( File.Exists( target ) )
+            {
+               File.Delete( target );
+            }
+
+            File.Copy( filePath, target );
+            newPath = target;
+            return true;
+         }
+         catch
+         {
+            newPath = string.Empty;
+            return false;
+         }
       }
 
-      public static string RenameFile( string filePath, string newName )
+      public static bool RenameFile( string filePath, string newName, out string newPath )
       {
-         var dir = Path.GetDirectoryName( filePath );
-         var newFilePath = Path.Combine( dir, newName ) + Path.GetExtension( filePath );
-
-         if ( File.Exists( newFilePath ) )
+         try
          {
-            File.Delete( newFilePath );
-         }
+            var dir = Path.GetDirectoryName( filePath );
+            var newFilePath = Path.Combine( dir, newName ) + Path.GetExtension( filePath );
 
-         File.Move( filePath, newFilePath );
-         return newFilePath;
+            if ( File.Exists( newFilePath ) )
+            {
+               File.Delete( newFilePath );
+            }
+
+            File.Move( filePath, newFilePath );
+            newPath = newFilePath;
+            return true;
+         }
+         catch
+         {
+            newPath = string.Empty;
+            return false;
+         }
       }
    }
 }
