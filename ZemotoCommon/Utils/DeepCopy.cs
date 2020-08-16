ebf460a168cea66b6a87ba57e7ae5a2644dad28d
David <none@none.com>
2020-08-16 17:41:41 -0400

    Add utility class to deep copy any given object

---
+++ ZemotoCommon/Utils/DeepCopy.cs
@@ -0,0 +1,96 @@
+﻿// Based on https://stackoverflow.com/questions/129389/how-do-you-do-a-deep-copy-of-an-object-in-net/11308879#11308879
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ZemotoCommon.Utils
+{
+   public static class DeepCopy
+   {
+      private static readonly MethodInfo CloneMethod = typeof( object ).GetMethod( "MemberwiseClone", BindingFlags.NonPublic | BindingFlags.Instance );
+
+      private static bool IsPrimitive( Type type )
+      {
+         if ( type == typeof( string ) )
+         {
+            return true;
+         }
+
+         return type.IsValueType & type.IsPrimitive;
+      }
+
+      private static object InternalCopy( object originalObject, IDictionary<object, object> visited )
+      {
+         if ( originalObject == null )
+         {
+            return null;
+         }
+
+         var typeToReflect = originalObject.GetType();
+         if ( IsPrimitive( typeToReflect ) )
+         {
+            return originalObject;
+         }
+
+         if ( visited.ContainsKey( originalObject ) )
+         {
+            return visited[originalObject];
+         }
+
+         if ( typeof( Delegate ).IsAssignableFrom( typeToReflect ) )
+         {
+            return null;
+         }
+
+         var cloneObject = CloneMethod.Invoke( originalObject, null );
+         if ( typeToReflect.IsArray )
+         {
+            var arrayType = typeToReflect.GetElementType();
+            if ( IsPrimitive( arrayType ) == false )
+            {
+               Array clonedArray = (Array)cloneObject;
+               clonedArray.ForEach( ( array, indices ) => array.SetValue( InternalCopy( clonedArray.GetValue( indices ), visited ), indices ) );
+            }
+         }
+
+         visited.Add( originalObject, cloneObject );
+         CopyFields( originalObject, visited, cloneObject, typeToReflect );
+         RecursiveCopyBaseTypePrivateFields( originalObject, visited, cloneObject, typeToReflect );
+
+         return cloneObject;
+      }
+
+      private static void RecursiveCopyBaseTypePrivateFields( object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect )
+      {
+         if ( typeToReflect.BaseType != null )
+         {
+            RecursiveCopyBaseTypePrivateFields( originalObject, visited, cloneObject, typeToReflect.BaseType );
+            CopyFields( originalObject, visited, cloneObject, typeToReflect.BaseType, BindingFlags.Instance | BindingFlags.NonPublic, info => info.IsPrivate );
+         }
+      }
+
+      private static void CopyFields( object originalObject, IDictionary<object, object> visited, object cloneObject, Type typeToReflect, BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy, Func<FieldInfo, bool> filter = null )
+      {
+         foreach ( FieldInfo fieldInfo in typeToReflect.GetFields( bindingFlags ) )
+         {
+            if ( filter != null && filter( fieldInfo ) == false )
+            {
+               continue;
+            }
+
+            if ( IsPrimitive( fieldInfo.FieldType ) )
+            {
+               continue;
+            }
+
+            var originalFieldValue = fieldInfo.GetValue( originalObject );
+            var clonedFieldValue = InternalCopy( originalFieldValue, visited );
+            fieldInfo.SetValue( cloneObject, clonedFieldValue );
+         }
+      }
+
+      public static T Copy<T>( T original ) => (T)Copy( (object)original );
+
+      private static object Copy( object originalObject ) => InternalCopy( originalObject, new Dictionary<object, object>( new ReferenceEqualityComparer() ) );
+   }
+}
