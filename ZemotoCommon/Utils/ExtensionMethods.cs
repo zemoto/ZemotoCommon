632e0e023fea968e1b2acca9951fb08dbf5799b0
David <none@none.com>
2020-08-16 17:41:23 -0400

    Add foreach extension method for multidimensional arrays

--- ZemotoCommon/Utils/ExtensionMethods.cs
+++ ZemotoCommon/Utils/ExtensionMethods.cs
@@ -41,5 +41,53 @@ namespace ZemotoCommon.Utils
       {
          return property.GetCustomAttributes( typeof( T ), false ).Cast<T>();
       }
+
+      public static void ForEach( this Array array, Action<Array, int[]> action )
+      {
+         if ( array.LongLength == 0 )
+         {
+            return;
+         }
+
+         var walker = new ArrayTraverse( array );
+         do
+         {
+            action( array, walker.Position );
+         }
+         while ( walker.Step() );
+      }
+
+      internal sealed class ArrayTraverse
+      {
+         public int[] Position { get; private set; }
+         private int[] _maxLengths;
+
+         public ArrayTraverse( Array array )
+         {
+            _maxLengths = new int[array.Rank];
+            for ( int i = 0; i < array.Rank; ++i )
+            {
+               _maxLengths[i] = array.GetLength( i ) - 1;
+            }
+            Position = new int[array.Rank];
+         }
+
+         public bool Step()
+         {
+            for ( int i = 0; i < Position.Length; ++i )
+            {
+               if ( Position[i] < _maxLengths[i] )
+               {
+                  Position[i]++;
+                  for ( int j = 0; j < i; j++ )
+                  {
+                     Position[j] = 0;
+                  }
+                  return true;
+               }
+            }
+            return false;
+         }
+      }
    }
 }
