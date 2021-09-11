ca76d5b172551beb1113510cf22fa571ac1e105f
Zemoto <none@none.com>
2021-09-11 12:15:04 -0400

    Move file-related utility methods to FileUtils

--- ZemotoCommon/Utils/FileUtils.cs
+++ ZemotoCommon/Utils/FileUtils.cs
@@ -84,5 +84,33 @@ namespace ZemotoCommon.Utils
             return false;
          }
       }
+
+      public static string MakeUniqueFileName( string filePath )
+      {
+         string dir = Path.GetDirectoryName( filePath );
+         string fileName = Path.GetFileNameWithoutExtension( filePath );
+         string fileExt = Path.GetExtension( filePath );
+
+         for ( int i = 1; ; i++ )
+         {
+            if ( !File.Exists( filePath ) )
+            {
+               return filePath;
+            }
+            filePath = Path.Combine( dir, $"{fileName}({i}){fileExt}" );
+         }
+      }
+
+      public static void SafeDeleteFile( string filePath )
+      {
+         if ( File.Exists( filePath ) )
+         {
+            try
+            {
+               File.Delete( filePath );
+            }
+            catch { /*ignored*/ }
+         }
+      }
    }
 }
--- ZemotoCommon/Utils/UtilityMethods.cs
+++ ZemotoCommon/Utils/UtilityMethods.cs
@@ -4,18 +4,6 @@ namespace ZemotoCommon.Utils
 {
    public static class UtilityMethods
    {
-      public static void SafeDeleteFile( string filePath )
-      {
-         if ( File.Exists( filePath ) )
-         {
-            try
-            {
-               File.Delete( filePath );
-            }
-            catch { /*ignored*/ }
-         }
-      }
-
       public static void SafeDeleteDirectory( string dirPath )
       {
          if ( Directory.Exists( dirPath ) )
@@ -33,22 +21,6 @@ namespace ZemotoCommon.Utils
          if ( !Directory.Exists( dirPath ) )
          {
             Directory.CreateDirectory( dirPath );
-         }
-      }
-
-      public static string MakeUniqueFileName( string filePath )
-      {
-         string dir = Path.GetDirectoryName( filePath );
-         string fileName = Path.GetFileNameWithoutExtension( filePath );
-         string fileExt = Path.GetExtension( filePath );
-
-         for ( int i = 1; ; i++ )
-         {
-            if ( !File.Exists( filePath ) )
-            {
-               return filePath;
-            }
-            filePath = Path.Combine( dir, $"{fileName}({i}){fileExt}" );
          }
       }
    }
