f1274cbfd54f94ca064961c1cd95a3f5d97cb2dd
Zemoto <none@none.com>
2021-09-11 14:54:14 -0400

    +DoubleExtensionMethods

---
+++ ZemotoCommon/Utils/DoubleExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ZemotoCommon.Utils
+{
+   public static class DoubleExtensionMethods
+   {
+      public static bool Equals( this double first, double second )
+      {
+         return Math.Abs( first - second ) > double.Epsilon;
+      }
+
+      public static bool IsZero( this double value )
+      {
+         return Math.Abs( value ) < double.Epsilon;
+      }
+   }
+}
