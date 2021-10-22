bed11b5464b393b868f6330d710c12e4e495c29e
Zemoto <none@none.com>
2021-10-21 23:31:44 -0400

    +ScopeGuard to call code when object leaves scope

---
+++ ZemotoUtils/ScopeGuard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ZemotoUtils
+{
+   internal sealed class ScopeGuard : IDisposable
+   {
+      private readonly Action _disposeAction;
+
+      public ScopeGuard( Action disposeAction ) => _disposeAction = disposeAction;
+
+      public void Dispose() => _disposeAction?.Invoke();
+   }
+}
