456e3a8ac1ea6e4c35261da8eab01b16e1d26cd9
zemoto <zemoto@gmail.com>
2022-10-29 16:36:11 -0400

    +SingleInstance to prevent multiple instances of running app

---
+++ ZemotoCommon/SingleInstance.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading;
+
+namespace ZemotoCommon
+{
+   internal sealed class SingleInstance
+   {
+      public event EventHandler PingedByOtherProcess;
+
+      private readonly Mutex _instanceMutex;
+      private readonly string _instanceName;
+
+      public SingleInstance( string instanceName )
+      {
+         _instanceMutex = new Mutex( true, instanceName );
+         _instanceName = instanceName;
+      }
+
+      public bool Claim()
+      {
+         if ( !_instanceMutex.WaitOne( TimeSpan.Zero ) )
+         {
+            PingSingleInstance();
+            return false;
+         }
+
+         ListenForOtherProcesses();
+         return true;
+      }
+
+      private void PingSingleInstance()
+      {
+         // The act of connecting indicates to the single instance that another process tried to run
+         using var client = new NamedPipeClientStream( ".", _instanceName, PipeDirection.Out );
+         try
+         {
+            client.Connect( 0 );
+         }
+         catch
+         {
+            // ignore
+         }
+      }
+
+      private void ListenForOtherProcesses()
+      {
+         var server = new NamedPipeServerStream( _instanceName, PipeDirection.In, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous );
+         _ = server.BeginWaitForConnection( OnPipeConnection, server );
+      }
+
+      private void OnPipeConnection( IAsyncResult ar )
+      {
+         using ( var server = (NamedPipeServerStream)ar.AsyncState )
+         {
+            try
+            {
+               server.EndWaitForConnection( ar );
+            }
+            catch
+            {
+               // ignore
+            }
+         }
+
+         PingedByOtherProcess?.Invoke( null, EventArgs.Empty );
+
+         ListenForOtherProcesses();
+      }
+   }
+}
