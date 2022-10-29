256f8fe127ce47c28d514813662e447d6a739980
zemoto <zemoto@gmail.com>
2022-10-29 19:01:25 -0400

    +SafeMethod static class for wrapping methods in try/catch

---
+++ ZemotoCommon/SafeMethod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ZemotoCommon
+{
+   internal static class SafeMethod
+   {
+      public static async Task InvokeSafely( Func<Task> action )
+      {
+         try
+         {
+            await action();
+         }
+         catch { /*ignored*/ }
+      }
+   }
+}
