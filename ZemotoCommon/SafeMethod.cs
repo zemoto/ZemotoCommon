933d9ec103f8a83e2a7a4121d2419d3d0ac05ef1
David <zemoto@gmail.com>
2023-06-05 16:18:50 -0400

    Expand SafeMethod to allow for actions to be take on exception
    And for the action to return a value

--- ZemotoCommon/SafeMethod.cs
+++ ZemotoCommon/SafeMethod.cs
@@ -5,12 +5,29 @@ namespace ZemotoCommon;
 
 internal static class SafeMethod
 {
-   public static async Task InvokeSafely( Func<Task> action )
+   public static async Task InvokeSafelyAsync( Task task, Action<Exception> exceptionAction = null )
    {
       try
       {
-         await action().ConfigureAwait( false );
+         await task.ConfigureAwait( false );
       }
-      catch { /*ignored*/ }
+      catch ( Exception ex )
+      {
+         exceptionAction?.Invoke( ex );
+      }
+   }
+
+   public static async Task<T> InvokeSafelyAsync<T>( Task<T> task, Action<Exception> exceptionAction = null )
+   {
+      try
+      {
+         return await task.ConfigureAwait( false );
+      }
+      catch ( Exception ex )
+      {
+         exceptionAction?.Invoke( ex );
+      }
+
+      return default;
    }
 }
