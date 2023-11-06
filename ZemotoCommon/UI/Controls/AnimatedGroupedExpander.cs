ba2495c6071d45a5636e1a9c60e9232cbbde0ba5
David <zemoto@gmail.com>
2023-11-06 17:04:48 -0500

    GroupedExpander => AnimatedGroupedExpander
    Add animation logic (if the provided template has the correct parts) to the expander so the content displays in a drawer-style animation.

---
+++ ZemotoCommon/UI/Controls/AnimatedGroupedExpander.cs
@@ -0,0 +1,126 @@
+﻿#if ZEMOTOUI
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Linq;
+using System;
+using System.Windows.Media.Animation;
+
+namespace ZemotoCommon.UI.Controls;
+
+[TemplatePart( Name = "PART_ContentContainer", Type = typeof( Panel ) )]
+[TemplatePart( Name = "PART_Content", Type = typeof( FrameworkElement ) )]
+internal sealed class AnimatedGroupedExpander : Expander
+{
+   private static readonly Dictionary<string, List<AnimatedGroupedExpander>> ExpanderGroups = new();
+   private static readonly TimeSpan _animationDuration = TimeSpan.FromMilliseconds( 200 );
+
+   public static readonly DependencyProperty GroupNameProperty = DependencyProperty.Register( nameof( GroupName ), typeof( string ), typeof( AnimatedGroupedExpander ), new PropertyMetadata( string.Empty, OnGroupNameChanged ) );
+   private static void OnGroupNameChanged( DependencyObject sender, DependencyPropertyChangedEventArgs e )
+   {
+      if ( sender is not AnimatedGroupedExpander expander )
+      {
+         return;
+      }
+
+      if ( e.OldValue is string oldGroupName && !string.IsNullOrEmpty( oldGroupName ) )
+      {
+         var group = ExpanderGroups[oldGroupName];
+         group.Remove( expander );
+
+         if ( group.Count == 0 )
+         {
+            ExpanderGroups.Remove( oldGroupName );
+         }
+      }
+
+      if ( e.NewValue is string newGroupName && !string.IsNullOrEmpty( newGroupName ) )
+      {
+         if ( !ExpanderGroups.ContainsKey( newGroupName ) )
+         {
+            ExpanderGroups[newGroupName] = new List<AnimatedGroupedExpander>();
+         }
+
+         ExpanderGroups[newGroupName].Add( expander );
+      }
+   }
+
+   public string GroupName
+   {
+      get => (string)GetValue( GroupNameProperty );
+      set => SetValue( GroupNameProperty, value );
+   }
+
+   private Panel _contentContainer;
+   private FrameworkElement _contentControl;
+
+   public AnimatedGroupedExpander()
+   {
+      Expanded += OnExpanded;
+      Collapsed += OnCollapsed;
+      IsKeyboardFocusWithinChanged += OnKeyboardFocusWithinChanged;
+   }
+
+   private void OnExpanded( object sender, RoutedEventArgs e )
+   {
+      if ( !string.IsNullOrEmpty( GroupName ) )
+      {
+         foreach ( var expander in ExpanderGroups[GroupName].Where( expander => expander != this ) )
+         {
+            expander.IsExpanded = false;
+         }
+      }
+
+      Animate();
+   }
+
+   private void OnCollapsed( object sender, RoutedEventArgs e ) => Animate();
+
+   private void OnKeyboardFocusWithinChanged( object sender, DependencyPropertyChangedEventArgs e )
+   {
+      if ( IsKeyboardFocusWithin && !IsExpanded )
+      {
+         IsExpanded = true;
+      }
+   }
+
+   public override void OnApplyTemplate()
+   {
+      base.OnApplyTemplate();
+      if ( _contentControl is not null )
+      {
+         _contentControl.SizeChanged -= OnContentControlSizeChanged;
+      }
+      if ( Template is null )
+      {
+         return;
+      }
+
+      _contentContainer = Template.FindName( "PART_ContentContainer", this ) as Panel;
+      _contentControl = Template.FindName( "PART_Content", this ) as FrameworkElement;
+      if ( _contentContainer is not null && _contentControl is not null )
+      {
+         _contentContainer.Height = IsExpanded ? _contentControl.ActualHeight : 0;
+         _contentControl.SizeChanged += OnContentControlSizeChanged;
+      }
+   }
+
+   private void OnContentControlSizeChanged( object sender, SizeChangedEventArgs e )
+   {
+      if ( IsExpanded )
+      {
+         // Setting the height directly does not work here for some reason, so use a zero duration animation
+         _contentContainer.BeginAnimation( HeightProperty, new DoubleAnimation( _contentControl.ActualHeight, TimeSpan.Zero ) );
+      }
+   }
+
+   private void Animate()
+   {
+      if ( _contentContainer is not null && _contentControl is not null )
+      {
+         var animation = new DoubleAnimation( _contentContainer.Height, IsExpanded ? _contentControl.ActualHeight : 0, _animationDuration );
+         _contentContainer.BeginAnimation( HeightProperty, animation );
+      }
+   }
+}
+#endif
