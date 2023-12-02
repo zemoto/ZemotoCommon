9b5a7917ff5bbc7f008137587268c853513fffe7
David <zemoto@gmail.com>
2023-12-02 12:43:04 -0500

    +CancelTokenProvider
    Class that will allow for repeated grabbing of a new token so that the owning class won't have to handle disposing/recreating for repeated cancellable tasks.

---
+++ ZemotoCommon/CancelTokenProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ZemotoCommon;
+
+internal sealed class CancelTokenProvider : IDisposable
+{
+   private CancellationTokenSource _tokenSource = new();
+
+   public CancellationToken GetToken()
+   {
+      if ( _tokenSource.Token.IsCancellationRequested )
+      {
+         _tokenSource.Dispose();
+         _tokenSource = new CancellationTokenSource();
+      }
+
+      return _tokenSource.Token;
+   }
+
+   public void Cancel() => _tokenSource.Cancel();
+
+   public void Dispose() => _tokenSource.Dispose();
+}
