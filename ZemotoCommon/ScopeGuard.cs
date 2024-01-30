8d766e2cda0a309045342b9980dbca7b3670adce
David <zemoto@gmail.com>
2024-01-30 18:21:25 -0500

    Add a construction action to ScopeGuard
    Has no real functional purpose, just makes the code read better when using ScopeGuard

--- ZemotoCommon/ScopeGuard.cs
+++ ZemotoCommon/ScopeGuard.cs
@@ -2,7 +2,17 @@
 
 namespace ZemotoCommon;
 
-internal sealed class ScopeGuard( Action disposeAction ) : IDisposable
+internal sealed class ScopeGuard : IDisposable
 {
-   public void Dispose() => disposeAction?.Invoke();
+   private readonly Action _disposeAction;
+
+   public ScopeGuard( Action constructionAction, Action disposeAction )
+   {
+      constructionAction.Invoke();
+      _disposeAction = disposeAction;
+   }
+
+   public ScopeGuard( Action disposeAction ) => _disposeAction = disposeAction;
+
+   public void Dispose() => _disposeAction?.Invoke();
 }
