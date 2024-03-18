c0dbfeb488a0846a7f328d58ecb98aee369d11f6
David <zemoto@gmail.com>
2024-03-18 16:04:12 -0400

    +CommonApp
    Base Application class that handles SingleInstance logic and writing out a crash file on unhandled exception

---
+++ ZemotoCommon/UI/CommonApp.cs
@@ -0,0 +1,29 @@
+﻿#if ZEMOTOUI
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace ZemotoCommon.UI;
+
+internal abstract class CommonApp : Application, IDisposable
+{
+   private readonly SingleInstance _singleInstance;
+
+   protected CommonApp( string instanceName, bool listenForOtherInstances )
+   {
+      DispatcherUnhandledException += OnUnhandledException;
+      _singleInstance = new SingleInstance( instanceName, listenForOtherInstances );
+      if ( !_singleInstance.Claim() )
+      {
+         Shutdown();
+      }
+   }
+
+   public void Dispose() => _singleInstance.Dispose();
+
+   protected override void OnExit( ExitEventArgs e ) => Dispose();
+
+   private void OnUnhandledException( object sender, DispatcherUnhandledExceptionEventArgs e ) => File.WriteAllText( "crash.txt", e.Exception.ToString() );
+}
+#endif
