b5b1ab4c59c61d7aa23ba26736ef2b1b9f5efc6e
David <zemoto@gmail.com>
2024-07-25 08:53:06 -0400

    File-scoped namespace on CustomProperties

--- ZemotoCommon/UI/CustomProperties.cs
+++ ZemotoCommon/UI/CustomProperties.cs
@@ -2,17 +2,16 @@
 using System.Windows;
 using System.Windows.Media;
 
-namespace ZemotoCommon.UI
-{
-   internal static class CustomProperties
-   {
-      public static readonly DependencyProperty MouseOverBrushProperty = DependencyProperty.RegisterAttached( "MouseOverBrush", typeof( SolidColorBrush ), typeof( CustomProperties ), new FrameworkPropertyMetadata( null ) );
-      public static SolidColorBrush GetMouseOverBrush( UIElement target ) => (SolidColorBrush)target.GetValue( MouseOverBrushProperty );
-      public static void SetMouseOverBrush( UIElement target, SolidColorBrush value ) => target.SetValue( MouseOverBrushProperty, value );
+namespace ZemotoCommon.UI;
 
-      public static readonly DependencyProperty PressedBrushProperty = DependencyProperty.RegisterAttached( "PressedBrush", typeof( SolidColorBrush ), typeof( CustomProperties ), new FrameworkPropertyMetadata( null ) );
-      public static SolidColorBrush GetPressedBrush( UIElement target ) => (SolidColorBrush)target.GetValue( PressedBrushProperty );
-      public static void SetPressedBrush( UIElement target, SolidColorBrush value ) => target.SetValue( PressedBrushProperty, value );
-   }
+internal static class CustomProperties
+{
+   public static readonly DependencyProperty MouseOverBrushProperty = DependencyProperty.RegisterAttached( "MouseOverBrush", typeof( SolidColorBrush ), typeof( CustomProperties ), new FrameworkPropertyMetadata( null ) );
+   public static SolidColorBrush GetMouseOverBrush( UIElement target ) => (SolidColorBrush)target.GetValue( MouseOverBrushProperty );
+   public static void SetMouseOverBrush( UIElement target, SolidColorBrush value ) => target.SetValue( MouseOverBrushProperty, value );
+
+   public static readonly DependencyProperty PressedBrushProperty = DependencyProperty.RegisterAttached( "PressedBrush", typeof( SolidColorBrush ), typeof( CustomProperties ), new FrameworkPropertyMetadata( null ) );
+   public static SolidColorBrush GetPressedBrush( UIElement target ) => (SolidColorBrush)target.GetValue( PressedBrushProperty );
+   public static void SetPressedBrush( UIElement target, SolidColorBrush value ) => target.SetValue( PressedBrushProperty, value );
 }
 #endif
