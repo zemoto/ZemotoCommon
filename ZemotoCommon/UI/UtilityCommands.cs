94444937c87b7627652c8e12680da87991ffa99e
David <zemoto@gmail.com>
2024-10-13 11:09:56 -0400

    +UtilityMethods and UtilityCommands to expose those methods to UI

---
+++ ZemotoCommon/UI/UtilityCommands.cs
@@ -0,0 +1,11 @@
+﻿#if ZEMOTOUI
+using CommunityToolkit.Mvvm.Input;
+
+namespace ZemotoCommon.UI;
+
+internal static class UtilityCommands
+{
+   private static RelayCommand<string> _openInBrowserCommand;
+   public static RelayCommand<string> OpenInBrowserCommand => _openInBrowserCommand ??= new RelayCommand<string>( UtilityMethods.OpenInBrowser );
+}
+#endif
---
+++ ZemotoCommon/UtilityMethods.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+namespace ZemotoCommon;
+
+internal static class UtilityMethods
+{
+   public static void OpenInBrowser( string url ) => Process.Start( new ProcessStartInfo( url ) { UseShellExecute = true } );
+}
