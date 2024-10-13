8eb8c11d7c9093a01d29d531909899a088214752
David <zemoto@gmail.com>
2024-10-13 11:13:53 -0400

    Merge DoubleExtensionMethods into ExtensionMethods

--- ZemotoCommon/ExtensionMethods.cs
+++ ZemotoCommon/ExtensionMethods.cs
@@ -8,6 +8,21 @@ namespace ZemotoCommon;
 
 internal static class ExtensionMethods
 {
+   public static bool IsEqualTo( this double first, double second )
+   {
+      return Math.Abs( first - second ) < double.Epsilon;
+   }
+
+   public static bool IsZero( this double value )
+   {
+      return Math.Abs( value ) < double.Epsilon;
+   }
+
+   public static double MapNumberToRange( this double value, double oldMin, double oldMax, double newMin, double newMax )
+   {
+      return ( ( value - oldMin ) / ( oldMax - oldMin ) * ( newMax - newMin ) ) + newMin;
+   }
+
    public static void StartAsChildProcess( this Process process )
    {
       ArgumentNullException.ThrowIfNull( process );
