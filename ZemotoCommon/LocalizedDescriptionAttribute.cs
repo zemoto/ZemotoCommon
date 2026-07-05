d245ece77c6a1af57f83602320d54f82e424c58e
David <zemoto@gmail.com>
2026-07-05 17:14:18 -0400

    +LocalizedDescriptionAttribute
    To allow for use of localizable strings in Description attribute, mainly for the purpose of providing string values to display in place of enum values.

---
+++ ZemotoCommon/LocalizedDescriptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Resources;
+
+namespace ZemotoCommon;
+
+/// <summary>
+/// Allows use of localized strings in a DescriptionAttribute.
+/// </summary>
+/// <param name="resourceKey">Key of the resource in string form</param>
+/// <param name="resourceType">The type of the class containing the resources</param>
+internal sealed class LocalizedDescriptionAttribute( string resourceKey, Type resourceType ) : DescriptionAttribute
+{
+   private readonly ResourceManager _resourceManager = new( resourceType );
+
+   public override string Description
+   {
+      get
+      {
+         string? description = _resourceManager.GetString( resourceKey );
+         return string.IsNullOrEmpty( description ) ? resourceKey : description;
+      }
+   }
+}
